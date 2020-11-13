f10d9764b1e03db9c8a32af5e748cc73e54af982
santigut123 <santigut123@gmail.com>
2020-11-13 12:13:34 -0500

    added basic layouts for html css and javascript file. javascript file has layout of program in comments

---
+++ game.js
@@ -0,0 +1,21 @@
+// *IIFE function for the board*
+// contains 2d array that holds the status for each of the squares 
+// contains function that changes the status of a square, takes in symbol
+// contains function to reset the board 
+// contains function that checks for victory
+
+//Object Factory for player
+//Contains player name.
+//Contains symbol for player
+//Contains logic to check board to see if square is empty. if it isnt, continues until player chooses empty square
+//Contains logic to emphasis that player is on a certain turn
+
+//IIFE function for the game 
+//track number of moves
+//makes 2 players
+//gets symbols based on input for the players.
+//calls players to make moves, checks for valid moves
+//start checking victories after 3 moves
+// after 9 moves end game offer to reset board and play again
+
+
